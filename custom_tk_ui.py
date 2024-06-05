1b1abe24b3da25e88493a7194dffbb7120a2a8d9
Kevin Wang <kayhne999@gmail.com>
2024-06-05 17:44:18 -0400

    CTk UI now class-oriented
    For ease of importing
    create_app() creates an chatbox instance

--- custom_tk_ui.py
+++ custom_tk_ui.py
@@ -4,64 +4,80 @@ from PIL import Image # image processing
 
 bot_name = "Chat Control"
 
-app = CTk()
-app.geometry("400x980")
-app.title("Chat Control")
-set_appearance_mode("dark")
-title_label = CTkLabel(app, text="Chat Control",font=CTkFont(family="Helvectica", size=25, weight='bold'))
-title_label.pack(padx=10, pady=(20,20))
+# app = CTk()
+# app.geometry("400x980")
+# app.title("Chat Control")
+# set_appearance_mode("dark")
+# title_label = CTkLabel(app, text="Chat Control",font=CTkFont(family="Helvectica", size=25, weight='bold'))
+# title_label.pack(padx=10, pady=(20,20))
 
-def send_message(event=None):
-    msg1 = entry.get().strip()
-    if msg1:
-        textbox.configure(state="normal")
-        textbox.insert("end", f"You: {msg1}\n")
-        textbox.configure(state="disabled")
-        textbox.see("end")
-        entry.delete(0, "end")
-        
-        msg2 = "Hello! How may I assist you today?" # replace with chatbot output
-        textbox.configure(state="normal")
-        textbox.insert("end", f"CC: {msg2}\n")
-        textbox.configure(state="disabled")
-        textbox.see("end")
+class App():
+    def __init__(self):
+        self.app = CTk()
+        self.app.geometry("400x980")
+        self.app.title("Chat Control")
+        self.title_label = CTkLabel(self.app, text="Chat Control",font=CTkFont(family="Helvectica", size=25, weight='bold'))
+        self.title_label.pack(padx=10, pady=(20,20))
+        self._setup_main_window()
 
-def screenshot():
-    # Not done yet
-    textbox.configure(state="normal")
-    textbox.insert("end", f"You sent a screenshot.\n")
-    textbox.insert("end", "CC: Analyzing....\n") # replace with chatbot output
-    textbox.configure(state="disabled")
-    textbox.see("end")
+    def send_message(self, event=None):
+        msg1 = self.entry.get().strip()
+        if msg1:
+            self.textbox.configure(state="normal")
+            self.textbox.insert("end", f"You: {msg1}\n")
+            self.textbox.configure(state="disabled")
+            self.textbox.see("end")
+            self.entry.delete(0, "end")
+            
+            msg2 = "Hello! How may I assist you today?" # replace with chatbot output
+            self.textbox.configure(state="normal")
+            self.textbox.insert("end", f"CC: {msg2}\n")
+            self.textbox.configure(state="disabled")
+            self.textbox.see("end")
 
-send_icon = Image.open("airplane.png")
-btn = CTkButton(master=app, 
-                text="", 
-                hover_color="#4158D0", 
-                image=CTkImage(dark_image=send_icon, light_image=send_icon),
-                command=send_message)
-btn.place(relx=0.67, rely=0.90, relheight=0.06, relwidth=0.22)
+    def screenshot(self):
+        # Not done yet
+        self.textbox.configure(state="normal")
+        self.textbox.insert("end", f"You sent a screenshot.\n")
+        self.textbox.insert("end", "CC: Analyzing....\n") # replace with chatbot output
+        self.textbox.configure(state="disabled")
+        self.textbox.see("end")
 
-camera_icon = Image.open("camera.png")
-screenshot_btn = CTkButton(master=app, 
-                           text="", 
-                           hover_color="#4158D0", 
-                           image=CTkImage(dark_image=camera_icon, light_image=camera_icon),
-                           command=screenshot)
+    def _setup_main_window(self):
+        self.send_icon = Image.open("airplane.png")
+        self.btn = CTkButton(master=self.app, 
+                        text="", 
+                        hover_color="#4158D0", 
+                        image=CTkImage(dark_image=self.send_icon, light_image=self.send_icon),
+                        command=self.send_message)
+        self.btn.place(relx=0.67, rely=0.90, relheight=0.06, relwidth=0.22)
 
-screenshot_btn.place(relx=0.82, rely=0.90, relheight=0.06, relwidth=0.12,)
+        self.camera_icon = Image.open("camera.png")
+        self.screenshot_btn = CTkButton(master=self.app, 
+                                text="", 
+                                hover_color="#4158D0", 
+                                image=CTkImage(dark_image=self.camera_icon, light_image=self.camera_icon),
+                                command=self.screenshot)
+
+        self.screenshot_btn.place(relx=0.82, rely=0.90, relheight=0.06, relwidth=0.12,)
 
 
-entry = CTkEntry(master=app,
-                 placeholder_text="Need help?",
-                 width=300,
-                 text_color="#FFFFFF")
-entry.place(relwidth=0.70, relheight=0.06, rely=0.90, relx=0.011)
-entry.bind("<Return>", send_message)
-entry.focus_set()
+        self.entry = CTkEntry(master=self.app,
+                        placeholder_text="Need help?",
+                        width=300,
+                        text_color="#FFFFFF")
+        self.entry.place(relwidth=0.70, relheight=0.06, rely=0.90, relx=0.011)
+        self.entry.bind("<Return>", self.send_message)
+        # self.entry.focus_set()
 
-# textbox = CTkTextbox(master=app, scrollbar_button_color="#FFCC70", corner_radius=16)
-textbox = CTkTextbox(master=app, scrollbar_button_color="#FFFFFF", corner_radius=16, state="disabled")
-textbox.place(relheight=0.80, relwidth=1, rely=0.10)
+        # textbox = CTkTextbox(master=app, scrollbar_button_color="#FFCC70", corner_radius=16)
+        self.textbox = CTkTextbox(master=self.app, scrollbar_button_color="#FFFFFF", corner_radius=16, state="disabled")
+        self.textbox.place(relheight=0.80, relwidth=1, rely=0.10)
 
-app.mainloop()
+def create_app():
+    app = App()
+    set_appearance_mode("dark")
+    app.app.mainloop()
+
+if __name__ == "__main__":
+    create_app()
